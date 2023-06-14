f76374c7aee95edb76874cee13c4112c77d3b022
Priyambada Roul <roulpriyambada@gmail.com>
2023-06-14 21:51:17 +0530

    Initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
     return  (
         <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
             <div>
-                <h1 className="text-5xl font-signature ml-4">PriyaRoul</h1>
+                <h1 className="text-5xl font-medium ml-4">Priya Roul</h1>
             </div>
 
             <ul className="flex">
